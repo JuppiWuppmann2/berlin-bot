ec1177dda1fcf076cdf3d9d99588d4b63cf21c5a
JuppiWuppmann2 <x.rendergit@gmail.com>
2025-08-23 18:58:44 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, HTTPException
+import os
 from tweet import send_tweet
 
 app = FastAPI()
+API_KEY = os.getenv("API_KEY")
 
 @app.post("/tweet")
 async def tweet(request: Request):
+    auth = request.headers.get("Authorization")
+    if not auth or auth != f"Bearer {API_KEY}":
+        raise HTTPException(status_code=401, detail="Unauthorized")
+
     data = await request.json()
     text = data.get("text")
     if not text:
         return {"error": "No text provided"}
     await send_tweet(text)
     return {"status": "Tweet sent"}
+
