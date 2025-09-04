ed0cf71940a75ed46a372fe81a33c79ee8d064ff
JuppiWuppmann2 <x.rendergit@gmail.com>
2025-09-04 12:53:55 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ import os
 import json
 import time
 import requests
+from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
