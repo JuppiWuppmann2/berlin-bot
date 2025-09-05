e91889155d7b71e3a40f3558d0cd0aab41defe2f
JuppiWuppmann2 <x.rendergit@gmail.com>
2025-09-05 19:35:27 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,9 +9,9 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.support.ui import WebDriverWait
-from selenium.webdriver.common.support import expected_conditions as EC
-from selenium.webdriver.common.exceptions import TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 from webdriver_manager.chrome import ChromeDriverManager
 
 from beautify import beautify_text
